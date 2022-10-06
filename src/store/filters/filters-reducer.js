af3229de06a1dc0f861ac8d54ef7b9d6aae2d12a
Serhii <sergiomakafon@gmail.com>
2022-10-06 11:53:52 +0300

    added reducers, actions and selectors

---
+++ src/store/filters/filters-reducer.js
@@ -0,0 +1,16 @@
+import {ADD_FILTER, REMOVE_FILTER, CLEAR_FILTER} from './filters-actions';
+
+export const filterReducer = (state = [], action) => {
+   switch (action.type) {
+      case ADD_FILTER:
+         return [...state, action.filter]
+      case REMOVE_FILTER: {
+         return state.filter(el => el !== action.filter);
+      }
+      case CLEAR_FILTER:
+         return [];
+      default: {
+         return state;
+      }
+   }
+}
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,5 +1,8 @@
 import { combineReducers } from "redux";
+import {positionReducer} from './positions/position-reducer';
+import {filterReducer} from './positions/filters-reducer';
 
 export const rootReducer = combineReducers({
-   
+   positions: positionReducer,
+   filters: filterReducer 
 })
